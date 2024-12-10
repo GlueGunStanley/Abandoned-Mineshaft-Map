b3166d90405309c2c240ffcd056b099c24b83d67
GlueGunStanley <kasonstanley@gmail.com>
2024-12-10 09:46:54 -0500

    Established File Structure

---
+++ Abandoned-Mineshaft-Map/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+game 'gta5'
+this_is_a_map 'yes'
+
+author 'Stanley Development Studios'
+description 'Explore a long abandoned mineshaft in the hills'
+
+github 'https://github.com/GlueGunStanley/Abandoned-Mineshaft-Map'
+discord 'https://discord.com/invite/uCKZJed3Gq'
